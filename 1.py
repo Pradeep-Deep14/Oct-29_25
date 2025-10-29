b8bb703716f658a5dd35c67a5404918f1505edc7
Pradeep <deeppradeep1998@gmail.com>
2025-10-29 13:56:21 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+L=[2,3,4]
+M=[3,4,5]
+
+print(max(L)>max(M))
+
+# Output: False
+# Explanation: The maximum value in list L is 4, and the maximum value in list M is 5. 
+# Since 4 is not greater than 5, the expression max(L) > max(M) evaluates to False.
+
